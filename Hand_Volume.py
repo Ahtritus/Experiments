ca0741d236bd5cd713c1c6bd30d17c2b09ea9c9b
Ahtritus <sutirthabharatidas@gmail.com>
2021-04-14 20:25:42 +0530

    Update Hand_Volume.py

--- Hand_Volume.py
+++ Hand_Volume.py
@@ -7,9 +7,8 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
  
-################################
 wCam, hCam = 640, 480
-################################
+
  
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
@@ -22,22 +21,23 @@ devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
+
 # volume.GetMute()
 # volume.GetMasterVolumeLevel()
+
 volRange = volume.GetVolumeRange()
 minVol = volRange[0]
 maxVol = volRange[1]
 vol = 0
 volBar = 400
 volPer = 0
+
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
-    if len(lmList) != 0:
-        # print(lmList[4], lmList[8])
- 
+    if len(lmList) != 0: 
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
@@ -48,10 +48,7 @@ while True:
         cv2.circle(img, (cx, cy), 6, (255, 0, 255), cv2.FILLED)
  
         length = math.hypot(x2 - x1, y2 - y1)
-        # print(length)
- 
-        # Hand range 50 - 300
-        # Volume Range -65 - 0
+        
  
         vol = np.interp(length, [20, 180], [minVol, maxVol])
         volBar = np.interp(length, [20, 180], [400, 150])
